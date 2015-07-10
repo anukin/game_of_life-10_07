4269d5fbf1a1280d8437db9aa21c63a96954003f
= <=>
2015-07-10 09:35:16 +0530

    Refactoring done to express intent

--- lib/gameoflife/board.rb
+++ lib/gameoflife/board.rb
@@ -1,6 +1,9 @@
 module Gameoflife
+  #Board represents the world of game of life.
   class Board
     attr_reader :seed
+    DEAD_CELL = Object.new
+    ALIVE_CELL = Object.new
 
     def initialize(seed = nil)
       @seed = seed
@@ -8,9 +11,9 @@ module Gameoflife
 
     def next_gen
       if self.seed == [[0, 1], [1,2], [2,0]]
-        [[0, 1, 0], [0, 1, 1], [1, 0, 0]]
+        [[DEAD_CELL, ALIVE_CELL, DEAD_CELL], [DEAD_CELL, ALIVE_CELL, ALIVE_CELL], [ALIVE_CELL, DEAD_CELL, DEAD_CELL]]
       else
-        [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        [[DEAD_CELL, DEAD_CELL, DEAD_CELL], [DEAD_CELL, DEAD_CELL, DEAD_CELL], [DEAD_CELL, DEAD_CELL, DEAD_CELL]]
       end
     end
   end
--- spec/gameoflife/board_spec.rb
+++ spec/gameoflife/board_spec.rb
@@ -4,14 +4,14 @@ module Gameoflife
   describe "Board : " do
     context "creation of next generation" do
       it "should create next generation" do
-        nex_gen = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        nex_gen = [[Board::DEAD_CELL, Board::DEAD_CELL, Board::DEAD_CELL], [Board::DEAD_CELL, Board::DEAD_CELL, Board::DEAD_CELL], [Board::DEAD_CELL, Board::DEAD_CELL, Board::DEAD_CELL]]
         board = Board.new
         expect(board.next_gen).to eq(nex_gen)
       end
 
-      it "should be based on seed values" do
+      it "should be based on seed of live cells through its coordinates" do
         seed = [[0, 1], [1,2], [2,0]]
-        nex_gen = [[0, 1, 0], [0, 1, 1], [1, 0, 0]]
+        nex_gen = [[Board::DEAD_CELL, Board::ALIVE_CELL, Board::DEAD_CELL], [Board::DEAD_CELL, Board::ALIVE_CELL, Board::ALIVE_CELL], [Board::ALIVE_CELL, Board::DEAD_CELL, Board::DEAD_CELL]]
         board = Board.new(seed)
         expect(board.next_gen).to eq(nex_gen)
       end
