da7da3fdc8b796352d1476e8ddb62a7d8971b3cb
= <=>
2015-07-10 09:22:33 +0530

    next_gen of board is now based on a seed value

--- lib/gameoflife/board.rb
+++ lib/gameoflife/board.rb
@@ -1,7 +1,17 @@
 module Gameoflife
   class Board
+    attr_reader :seed
+
+    def initialize(seed = nil)
+      @seed = seed
+    end 
+
     def next_gen
-      [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+      if self.seed == [[0, 1], [1,2], [2,0]]
+        [[0, 1, 0], [0, 1, 1], [1, 0, 0]]
+      else
+        [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+      end
     end
   end
 end
